6c065ee0da98ff6f08cfeb10821148db450ad6f6
MateoLCS <mateusz.namysl0318@gmail.com>
2022-07-17 16:58:12 +0200

    Created basic classes, experimenting with recent searches

--- App.js
+++ App.js
@@ -13,20 +13,119 @@ const formatData = async (data) => {
 }
 
 const search = async (keywoard) => {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${keywoard}&appid=${API_KEY}`)
-    const data = await response.json()
-    const formatedData = await formatData(data)
-    return formatedData
+    const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${keywoard}&appid=${API_KEY}`
+    try{
+        const response = await fetch(endpoint, {mode: 'cors'})
+        const data = await response.json()
+        const formatedData = await formatData(data)
+        return formatedData
+    }catch(err){
+        console.log(err)
+        return null
+    }
+}
+
+const clearSearch = () => {
+    inputKeywoard.value = ''
 }
 
 searchButton.addEventListener('click', async () => {
     const keywoard = inputKeywoard.value
     const data = await search(keywoard)
-    console.log(data)
-
-    keywoard.value = '' 
+    const result = new Search(data)
+    console.log(result)
+    clearSearch() 
 })
 
+class Search {
+    data = null
+
+    constructor(data){
+        this.data = data
+    }
+
+    getName(){
+        return this.data.cityName
+    }
+
+    getWeather(){
+        return this.data.weather
+    }
+
+    getTemperature(){
+        return this.data.temperature
+    }
+
+    getFeelsLike(){
+        return this.data.feelsLike
+    }
+
+    getData(){
+        return this.data
+    }
+}
 
 
-search('Poznań').then(console.log)
+class RecentSearches {
+    recentSearches = []
+    constructor(inSearches){
+        if(inSearches === undefined){
+            this.recentSearches = []
+        }else{
+            this.recentSearches = inSearches
+        }
+    }
+
+    setSearches(data){
+        this.recentSearches = data
+    }
+
+    addSearch(keywoard){
+        if(!this.alreadySerched(keywoard)){
+            if(this.recentSearches.length >= 6){
+                this.recentSearches.shift()
+                this.recentSearches.push(keywoard)
+            }else{
+                this.recentSearches.push(keywoard)
+            }
+        }else{
+            return null
+        }
+    }
+
+    getRecentSearches(){
+        return this.recentSearches
+    }
+
+    alreadySerched(keywoard){
+        return this.recentSearches.some(search => search === keywoard)
+    }
+}
+
+class Storage{
+
+    static setSearches(searches){
+        localStorage.setItem('searches', JSON.stringify(searches))
+    }
+
+    static getSearches(){
+        const searches = Object.assign(new RecentSearches,JSON.parse(localStorage.getItem('searches')))
+        return searches
+    }
+
+    static setSearch(keywoard){
+        const searches = Storage.getSearches()
+        searches.addSearch(keywoard)
+        Storage.setSearches(searches)
+    }
+
+
+}
+
+const recents = ['Poznań', 'Junoszyno', 'Warszawa', 'Radom', 'Gdynia', 'Powidz']
+const test = new RecentSearches(recents)
+console.log(test.getRecentSearches())
+test.addSearch('Wilno')
+console.log(test.getRecentSearches())
+test.addSearch('Szczytniki')
+console.log(test.getRecentSearches())
