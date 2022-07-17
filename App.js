3bd791ad1fd838c8dfea6ce61d167a35c7859b06
MateoLCS <mateusz.namysl0318@gmail.com>
2022-07-17 15:49:39 +0200

    Created function to format API call response

--- App.js
+++ App.js
@@ -3,10 +3,20 @@ const searchButton = document.querySelector('#search')
 
 const API_KEY = 'cf16fe3bca02efc84a2c2b0fee237e66'
 
+const formatData = async (data) => {
+    const cityName = data.name
+    const weather = data.weather[0].main
+    const temperature = data.main.temp
+    const feelsLike = data.main.feels_like
+
+    return {cityName, weather, temperature, feelsLike}
+}
+
 const search = async (keywoard) => {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${keywoard}&appid=${API_KEY}`)
     const data = await response.json()
-    return data
+    const formatedData = await formatData(data)
+    return formatedData
 }
 
 searchButton.addEventListener('click', async () => {
@@ -17,3 +27,6 @@ searchButton.addEventListener('click', async () => {
     keywoard.value = '' 
 })
 
+
+
+search('Poznań').then(console.log)
